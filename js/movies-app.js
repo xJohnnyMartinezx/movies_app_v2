1eaedfd8920d1a236a6b7bf412b1f3e1e9f73fff
Johnny <mrtnz.jn@gmail.com>
2023-04-11 23:04:26 -0500

    working on movie edit via modal functionality

--- js/movies-app.js
+++ js/movies-app.js
@@ -18,7 +18,7 @@ function loadMovies() {
     fetch("http://localhost:3000/movies")
         .then(resp => resp.json())
         .then(movieData => {
-            console.log(movieData)
+            // console.log(movieData)
            let moviesHTML = movieData.map(movie =>{
                 // language = html;
                 return `<div class="card" style="width: 18rem;">
@@ -26,7 +26,7 @@ function loadMovies() {
                         <div class="card-body">
                         <h5 class="card-title" id="movie-title">${movie.title}</h5>
                         <p class="card-text" id="movie-plot">${movie.plot}</p>
-                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal">Edit</button>
+                        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#myModal" onclick="populateEditModal(${movie.id})">Edit</button>
                       </div>
                     </div>
         `
@@ -40,15 +40,91 @@ function loadMovies() {
 
     // *********** EDIT MOVIE MODAL EVENT LISTENER ************
 
-    const myModal = document.getElementById('myModal')
-    const myInput = document.getElementById('myInput')
 
-    myModal.addEventListener('shown.bs.modal', () => {
-        myInput.focus()
-    })
+    //
+    // const myModal = document.getElementById('myModal')
+    // const userEditedTitle = document.getElementById('userEditedTitle')
+    // const userEditedDir = document.getElementById('userEditedDir')
+    // const userEditedYear = document.getElementById('userEditedYear')
+    //
+    //
+    // myModal.addEventListener('shown.bs.modal', () => {
+    //     userEditedTitle.focus()
+    //     userEditedDir.focus()
+    //     userEditedYear.focus()
+    // })
+
+function populateEditModal(id) {
+    fetch(`http://localhost:3000/movies/${id}`)
+        .then(resp => resp.json())
+        .then(movieData => {
+            console.log(movieData)
+            let editMoviesHTML = [movieData].map(movie => {
+                // console.log(movie.title);
+                return popUpModal(movie)
+            })
+            document.getElementById("myModal").innerHTML=editMoviesHTML;
+        })
+
+}
+
+// const myModal = document.getElementById('myModal')
+// const myInput = document.getElementById('myInput')
+// const userEditedDir = document.getElementById('userEditedDir')
+// const userEditedYear = document.getElementById('userEditedYear')
+
+
+// myModal.addEventListener('shown.bs.modal', () => {
+//     myInput.focus()
+//     // userEditedDir.focus()
+//     // userEditedYear.focus()
+// })
+
+function popUpModal(movie){
+    console.log(movie.title)
+    return `<!-- <div class="modal" id="myModal" tabIndex="-1">-->
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">${movie.title}</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <label for="userEditedTitle" class="form-label" style="color: black">Title</label>
+                <input id="userEditedTitle" type="text" placeholder="${movie.title}">
+                <label for="userEditedDir" class="form-label" style="color: black">Director</label>
+                <input id="userEditedDir" type="text" placeholder="${movie.director}">
+                <label for="userEditedYear" class="form-label" style="color: black">Year</label>
+                <input id="userEditedYear" type="text" placeholder="${movie.year}">
+                </div>
+
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                    </div>
+            </div>
+        </div>
+<!--    </div>-->`
+}
+
 // ************************************************************
 
+// EDIT MOVIE
 
+// function editMovieById(id){
+//         fetch(`http://localhost:3000/movies/${id}`, {
+//             method: "PATCH",
+//             headers: {
+//                 "Content-Type": "application/json"
+//             },
+//             body: JSON.stringify()
+//         })
+//             .then(resp => resp.json())
+//             .then(movieData => console.log(movieData))
+//             .catch(error => console.error(error));
+// }
+
+// function editMovieProperties();
 
 
 
